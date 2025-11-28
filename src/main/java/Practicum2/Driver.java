ac39a6b4d096294ce72c3ca9eefd3fcf0833f12e
siew_chen <Lsiewchen.21@gmail.com>
2025-11-28 19:12:54 +0800

    Removed code from Driver

--- src/main/java/Practicum2/Driver.java
+++ src/main/java/Practicum2/Driver.java
@@ -11,46 +11,6 @@ import jakarta.persistence.EntityManagerFactory;
 import java.time.LocalDate;
 
 public class Driver {
-//    static final String DBNAME = "employees";
-
     public static void main(String[] args){
-//        try (EntityManagerFactory emf = EMFactory.getInstance()) {
-//            EntityManager em = emf.createEntityManager();
-//            em.getTransaction().begin();
-
-//            Departments departments = new Departments();
-//            departments.setDeptNo("abc");
-//            departments.setDeptName("testDepartment");
-//            em.persist(departments);
-//            em.getTransaction().commit();
-
-//            Employees employees = new Employees();
-//            employees.setEmpNo(1);
-//            employees.setBirthDate(LocalDate.now());
-//            employees.setFirstName("Siew");
-//            employees.setLastName("Chen");
-//            employees.setGender(Gender.F);
-//            employees.setHireDate(LocalDate.now());
-//            em.persist(employees);
-//            em.getTransaction().commit();
-
-//            DeptEmp deptEmp = new DeptEmp();
-//            deptEmp.setDepartments(departments);
-//            deptEmp.setEmployees(employees);
-//            deptEmp.setFromDate(LocalDate.now());
-//            deptEmp.setToDate(LocalDate.now());
-//            em.persist(deptEmp);
-//            em.getTransaction().commit();
-
-//            Titles titles = new Titles();
-//            titles.setTitle("testTitle");
-//            titles.setFromDate(LocalDate.now());
-//            titles.setEmployees(employees);
-//            titles.setToDate(LocalDate.now());
-//            em.persist(titles);
-//            em.getTransaction().commit();
-
-//            em.close();
-//        }
     }
 }
